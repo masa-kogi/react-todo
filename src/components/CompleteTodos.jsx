2c7229b4694612abdaf83c3c065ef2c01fb02054
masa-kogi <59404286+masa-kogi@users.noreply.github.com>
2022-07-06 18:55:51 +0900

    Initial commit

---
+++ src/components/CompleteTodos.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const CompleteTodos = (props) => {
+  const { todos, onClickBack } = props;
+  return (
+    <div className="complete-area">
+      <p className="title">完了のTODO</p>
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={todo}>
+              <div className="list-row">
+                <p className="content">{todo}</p>
+                <button
+                  onClick={() => {
+                    onClickBack(index);
+                  }}
+                >
+                  戻す
+                </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
